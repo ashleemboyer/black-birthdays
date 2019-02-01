58d609a60b467f30dc4c53e962991fc98d103612
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2019-02-01 18:36:51 -0500

    Updates for mobile

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var is_mobile = (window.innerWidth || body.innerWidth) < 700;
+
 var body = document.body;
 
 var opaque = document.createElement('div');
@@ -13,39 +15,30 @@ opaque.style = `
 body.appendChild(opaque)
 
 var happy_birthday = document.createElement('div');
-happy_birthday.style = `
-  color: #000000;
-  font-size: 3rem;
-`;
+happy_birthday.style.color = '#000000';
+happy_birthday.style.fontSize = is_mobile ? '2.5rem' : '3rem';
 happy_birthday.innerHTML = 'Happy Birthday'
 body.appendChild(happy_birthday);
 
 var person_name = document.createElement('div');
-person_name.style = `
-  color: #000000;
-  font-size: 5rem;
-`;
+person_name.style.color = '#000000';
+person_name.style.fontSize = is_mobile ? '4.5rem' : '5rem';
 person_name.innerHTML = 'Langston Hughes'
 body.appendChild(person_name);
 
 var dates = document.createElement('div');
 dates.innerHTML = '2.1.1902 &mdash; 5.22.1967';
-dates.style = `
-  color: #000000;
-  font-size: 1.5rem;
-`;
+dates.style.color = '#000000';
+dates.style.fontSize = '1.5rem';
 body.appendChild(dates);
 
 var bio = document.createElement('div');
 bio.innerHTML = 'Attended Columbia University, but left after one year to travel. His poetry was later promoted by Vachel Lindsay, and Hughes published his first book in 1926. He went on to write countless works of poetry, prose and plays, as well as a popular column for the Chicago Defender.';
-bio.style = `
-  width: 50%;
-  color: #000000;
-  font-size: 1.5rem;
-  font-weight: 400;
-  line-height: 1.5;
-  margin: 32px 0;
-`;
+bio.style.width = is_mobile ? '100%' : '50%';
+bio.style.fontSize = is_mobile ? '1.2rem' : '1.5rem';
+bio.style.fontWeight = '400';
+bio.style.lineHeight = '1.5';
+bio.style.margin = is_mobile ? '16px 0' : '32px 0';
 body.appendChild(bio);
 
 var read_more = document.createElement('a');
@@ -54,7 +47,7 @@ read_more.href = 'https://www.biography.com/people/langston-hughes-9346313';
 read_more.style = `
   text-decoration: none;
   color: #000000;
-  font-weight: 400;
+  font-weight: bold;
   font-size: 1rem;
 `;
 body.appendChild(read_more);
