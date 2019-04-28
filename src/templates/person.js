1d387ad5bd5d2c48e6c49e81d929f59ed1f0f38a
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2019-04-28 19:44:29 -0400

    Removed hard-coded title to person page and fixed padding of content

--- src/templates/person.js
+++ src/templates/person.js
@@ -8,8 +8,8 @@ export default ({ data }) => {
   var person = data.allSitePage.edges[0].node.context
   return (
     <Layout>
-      <SEO title="Dennis Edwards" keywords={['gatsby', 'application', 'react', 'birthdays', 'black', 'history', 'month', 'dennis', 'edwards']} />
-      <div style={{ maxWidth: '700px', margin: '0 auto' }}>
+      <SEO title={person.name} keywords={['gatsby', 'application', 'react', 'birthdays', 'black', 'history', 'month', 'dennis', 'edwards']} />
+      <div style={{ maxWidth: '700px', margin: '0 auto', padding: '16px' }}>
         <h1>{person.name}</h1>
         <h3>{person.birth} &mdash; {person.death}</h3>
         <Image style={{ borderRadius: '4px' }} src={person.name.replace(/ |\./g, '') + '.jpg'} />
